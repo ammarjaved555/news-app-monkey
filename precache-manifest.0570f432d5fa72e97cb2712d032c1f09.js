f9c7f0212e552c1dda1a9a8bc4d2d7c297e55073
ammarjaved555 <ammarjaved555@gmail.com>
2022-11-18 19:11:09 +0500

    Updates

---
+++ precache-manifest.0570f432d5fa72e97cb2712d032c1f09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde97dd9cf00f06ad4da",
+    "url": "/news-app-monkey/static/css/main.ad63d893.chunk.css"
+  },
+  {
+    "revision": "dde97dd9cf00f06ad4da",
+    "url": "/news-app-monkey/static/js/main.dde97dd9.chunk.js"
+  },
+  {
+    "revision": "c625f3bb9e603565083e",
+    "url": "/news-app-monkey/static/js/1.c625f3bb.chunk.js"
+  },
+  {
+    "revision": "8c3e6cbeafc73e288fda",
+    "url": "/news-app-monkey/static/js/2.8c3e6cbe.chunk.js"
+  },
+  {
+    "revision": "aeb10bacf17a8247a575",
+    "url": "/news-app-monkey/static/js/runtime~main.aeb10bac.js"
+  },
+  {
+    "revision": "a10b52ee23b04ad778d1263272671544",
+    "url": "/news-app-monkey/static/media/loading.a10b52ee.gif"
+  },
+  {
+    "revision": "4fdff72aac1ff6c815c4715476b4400b",
+    "url": "/news-app-monkey/index.html"
+  }
+];
